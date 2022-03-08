b2ffb7db8737ef06cf1db503114afe2c849cb992
kangaro0 <johannes_herbert@yahoo.de>
2022-03-08 16:01:01 +0100

    updated blogcontroller to return updated object after writing to database

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -1,17 +1,7 @@
 require( 'dotenv' ).config()
 import { ObjectId, Document } from "mongodb";
 import BaseController from "./base.controller";
-import Post from '../interfaces/blog/post';
-
-const map_in_fnc = ( post: Post ): Document => {
-    return {
-        date: post.date,
-        draft: post.draft,
-        published: post.published,
-        title: post.title,
-        content: post.content
-    };
-};
+import Post from "../interfaces/blog/post";
 
 const map_out_fnc = ( doc: Document ): Post => {
     return {
@@ -30,15 +20,22 @@ export default class BlogController extends BaseController {
         super();
     }
 
-    public async getAll(): Promise<Array<Post>> {
+    public async getAll( page: number = 0, pageSize: number = 0 ): Promise<Array<Post>> {
         let items = new Array<Post>( 0 );
 
         try {
             await this.open();
 
             let collection = this.getCollection( process.env.COLLECTION_BLOG );
-
             let cursor = await collection.find( {} );
+
+            // implement pagination
+            let toSkip = page * pageSize;
+            cursor.skip( toSkip );
+
+            if( pageSize != 0 )
+                cursor.limit( pageSize );
+
             for await( const document of cursor ){
                 items.push( map_out_fnc( document ) );
             }
@@ -72,31 +69,39 @@ export default class BlogController extends BaseController {
     }
 
     public async insertOne( post: Post ): Promise<string> {
-        let insert = map_in_fnc( post );
         try {
             await this.open();
          
             let collection = this.getCollection( process.env.COLLECTION_BLOG );
-            await collection.insertOne( insert );
+            await collection.insertOne( post as Document );
 
             await this.close();
         } catch( err ){
             throw err;
         }
-        return insert._id;
+
+        return post._id as string;
     }
 
-    public async updateOne( id: string, post: Post ): Promise<void> {
+    public async updateOne( id: string, post: Post ): Promise<Post> {
+        // delete _id from object if it exists
+        if( post._id )
+            delete post._id;
+
         try { 
             await this.open();
 
             let collection = this.getCollection( process.env.COLLECTION_BLOG );
-            await collection.updateOne( { _id: new ObjectId( id ) }, { $set: map_in_fnc( post ) } );
+            await collection.updateOne( { _id: new ObjectId( id ) }, { $set: post } );
             
             await this.close();
         } catch( err ){
             throw err;
         }
+
+        // add it back & return updated object
+        post._id = id;
+        return post;
     }
 
     public async deleteOne( id: string ): Promise<void> {
@@ -110,5 +115,22 @@ export default class BlogController extends BaseController {
         } catch( err ){
             throw err;
         }
+    }
+
+    public async count( criteria: object ): Promise<number> {
+        let num = 0;
+        
+        try {
+            await this.open();
+
+            let collection = this.getCollection( process.env.COLLECTION_BLOG );
+            num = await collection.count( criteria );
+
+            await this.close();
+        } catch( err ){
+            throw err;
+        }
+
+        return num;
     }
 }
